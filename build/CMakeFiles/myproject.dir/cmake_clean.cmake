87958af8165de494404a68299c1c3512a5937ba5
Muslim Mamadaev <mamad.muslim@mail.ru>
2023-05-04 17:08:05 +0300

    add folder structure

---
+++ build/CMakeFiles/myproject.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myproject.dir/lib/server/src/server.cpp.o"
+  "CMakeFiles/myproject.dir/lib/server/src/server.cpp.o.d"
+  "myproject"
+  "myproject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myproject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
