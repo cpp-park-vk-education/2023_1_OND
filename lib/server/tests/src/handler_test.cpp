7a76e028c7dd1e77021244462d50eb3799e7d08b
Gvidow <arzamachev.art-alex@yandex.ru>
2023-05-06 23:55:47 +0300

    refactoring

--- lib/server/tests/src/handler_test.cpp
+++ lib/server/tests/src/handler_test.cpp
@@ -7,8 +7,8 @@ TEST(UserRegistrationTest, RegistrationNewUser) {
         rows{"username=user1\nmail=a@example.com\npassword=qwerty\ntoken=1234"});
     MockWriteSPtr mock = std::make_shared<MockWrite>();
     MockDatabaseSPtr db = std::make_shared<MockDatabase>();
-    std::shared_ptr<Handler> reg = std::make_shared<UserRegistration>(db);
-    EXPECT_CALL(*db, insertUser("user1", "a@example.com", "password=qwerty", "1234"));
+    HandlerSPtr reg = std::make_shared<UserRegistration>(db);
+    EXPECT_CALL(*db, insertUser("user1", "a@example.com", "qwerty", "1234"));
     reg->serve(mock, stub);
     EXPECT_EQ(mock->getData(), rows{"ok"});
 }
@@ -16,25 +16,31 @@ TEST(UserRegistrationTest, RegistrationNewUser) {
 TEST(UserRegistrationTest, RegistrationWithExistUsername) {
     StubReadSPtr stub = std::make_shared<StubRead>(
         rows{"username=user1\nmail=a@example.com\npassword=qwerty\ntoken=1234",
-             "username=user1\nmaiil=new@example.com\npassword=qwerty\ntoken=1234"});
+             "username=user1\nmail=new@example.com\npassword=qwerty\ntoken=1234"});
     MockWriteSPtr mock = std::make_shared<MockWrite>();
     MockDatabaseSPtr db = std::make_shared<MockDatabase>();
-    std::shared_ptr<Handler> reg = std::make_shared<UserRegistration>(db);
+    HandlerSPtr reg = std::make_shared<UserRegistration>(db);
+    EXPECT_CALL(*db, insertUser("user1", "a@example.com", "qwerty", "1234"));
+    reg->serve(mock, stub);
+    EXPECT_CALL(*db, insertUser("user1", "new@example.com", "qwerty", "1234"));
+    reg->serve(mock, stub);
     rows res = rows{"ok", "error"};
     EXPECT_EQ(mock->getData(), res);
-    reg->serve(mock, stub);
 }
 
 TEST(UserRegistrationTest, RegistrationWithExistMail) {
     StubReadSPtr stub = std::make_shared<StubRead>(
         rows{"username=user1\nmail=a@example.com\npassword=qwerty\ntoken=1234",
-             "username=user2\nmaiil=b@example.com\npassword=qwerty\ntoken=1234"});
+             "username=user2\nmail=a@example.com\npassword=qwerty\ntoken=1234"});
     MockWriteSPtr mock = std::make_shared<MockWrite>();
     MockDatabaseSPtr db = std::make_shared<MockDatabase>();
-    std::shared_ptr<Handler> reg = std::make_shared<UserRegistration>(db);
+    HandlerSPtr reg = std::make_shared<UserRegistration>(db);
+    EXPECT_CALL(*db, insertUser("user1", "a@example.com", "qwerty", "1234"));
+    reg->serve(mock, stub);
+    EXPECT_CALL(*db, insertUser("user2", "a@example.com", "qwerty", "1234"));
+    reg->serve(mock, stub);
     rows res = rows{"ok", "error"};
     EXPECT_EQ(mock->getData(), res);
-    reg->serve(mock, stub);
 }
 
 TEST(GetUserDataTest, GetDataExistUser) {
@@ -42,12 +48,10 @@ TEST(GetUserDataTest, GetDataExistUser) {
         rows{"username=admin"});
     MockWriteSPtr mock = std::make_shared<MockWrite>();
     MockDatabaseSPtr db = std::make_shared<MockDatabase>();
-    std::shared_ptr<Handler> reg = std::make_shared<GetUserData>(db);
+    std::shared_ptr<Handler> h = std::make_shared<GetUserData>(db);
     rows data;
     EXPECT_CALL(*db, selectUser("admin", data));
-    reg->serve(mock, stub);
-    rows res = rows{"ok"};
-    EXPECT_EQ(mock->getData(), res);
+    h->serve(mock, stub);
 }
 
 TEST(GetUserDataTest, GetDataNotExistUser) {
@@ -55,10 +59,45 @@ TEST(GetUserDataTest, GetDataNotExistUser) {
         rows{"username=1213"});
     MockWriteSPtr mock = std::make_shared<MockWrite>();
     MockDatabaseSPtr db = std::make_shared<MockDatabase>();
-    std::shared_ptr<Handler> reg = std::make_shared<GetUserData>(db);
+    std::shared_ptr<Handler> h = std::make_shared<GetUserData>(db);
     rows data;
     EXPECT_CALL(*db, selectUser("admin", data));
-    reg->serve(mock, stub);
+    h->serve(mock, stub);
     rows res = rows{"error"};
+    EXPECT_EQ(mock->getData(), res);
+}
+
+TEST(ChangeUserDataTest, ChangeData) {
+    StubReadSPtr stub = std::make_shared<StubRead>(
+        rows{"username=admin\nnew_username=user\nnew_password=ffff"});
+    MockWriteSPtr mock = std::make_shared<MockWrite>();
+    MockDatabaseSPtr db = std::make_shared<MockDatabase>();
+    std::shared_ptr<Handler> h = std::make_shared<GetUserData>(db);
+    EXPECT_CALL(*db, alterUser("admin", "user", "", "ffff", ""));
+    h->serve(mock, stub);
+}
+
+TEST(RemoveUserTest, RemoveExistsUser) {
+    StubReadSPtr stub = std::make_shared<StubRead>(
+        rows{"username=admin"});
+    MockWriteSPtr mock = std::make_shared<MockWrite>();
+    MockDatabaseSPtr db = std::make_shared<MockDatabase>();
+    std::shared_ptr<Handler> h = std::make_shared<GetUserData>(db);
+    EXPECT_CALL(*db, deleteUser("admin"));
+    h->serve(mock, stub);
+    EXPECT_EQ(mock->getData(), rows{"ok"});
+}
+
+TEST(RemoveUserTest, RemoveNotExistsUser) {
+    StubReadSPtr stub = std::make_shared<StubRead>(
+        rows{"username=admin", "username=admin"});
+    MockWriteSPtr mock = std::make_shared<MockWrite>();
+    MockDatabaseSPtr db = std::make_shared<MockDatabase>();
+    std::shared_ptr<Handler> h = std::make_shared<GetUserData>(db);
+    EXPECT_CALL(*db, deleteUser("admin"));
+    h->serve(mock, stub);
+    EXPECT_CALL(*db, deleteUser("admin"));
+    h->serve(mock, stub);
+    rows res{"ok", "error"};
     EXPECT_EQ(mock->getData(), res);
 }
