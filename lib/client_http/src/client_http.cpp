2745451c197dd7b2085a22af6822a9f6e7d679bd
Gvidow <arzamachev.art-alex@yandex.ru>
2023-05-14 12:24:50 +0300

    init server_mvp

--- lib/client_http/src/client_http.cpp
+++ lib/client_http/src/client_http.cpp
@@ -1,5 +1,12 @@
 #include "client_http.hpp"
+#include <iostream>
+#include <cpr/cpr.h>
 
 int ClientHTTP::post(const Header & headers, const std::string & body, const std::string & url, std::string & response) {
+    cpr::Response r = cpr::Post(cpr::Url{url},
+                                cpr::Header{headers},
+                                cpr::Body{body});
+    std::string answer = r.text;
+    std::cout << answer << std::endl;
     return 200;
 }
