6150ca1e13061d6e77fcb9942c885f14dacfd467
Khristina <hristinakirillova2559@gmail.com>
2023-05-16 17:44:11 +0300

    -build

--- Logic/build/CMakeFiles/client_logic.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libclient_logic.a"
-)
--- Logic/build/CMakeFiles/helloworld.dir/helloworld_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/helloworld.dir/helloworld_autogen/mocs_compilation.cpp.o: \
- /home/khristina/repos/YourVoice/Client/Logic/build/helloworld_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
