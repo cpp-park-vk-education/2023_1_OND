5d75a407f39093ec7ea178fd53aa266fc2f261e4
Muslim Mamadaev <mamad.muslim@mail.ru>
2023-05-16 17:03:24 +0300

    add ui and api classes

---
+++ lib/client/src/festival.cpp
@@ -0,0 +1,8 @@
+#include "../include/api.hpp" //need to fix:)
+#include <pocketsphinx.h>
+#include <fstream>
+#include <sstream>
+
+void APIFestival::speak(const std::string & text_answer, std::string & voice_answer){
+    //return 0;
+};
